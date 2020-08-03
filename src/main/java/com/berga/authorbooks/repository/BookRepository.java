3a74ecc03f24ed2dccebead88f7118f1fdd1f6c6
Carlos <carlos.bergamasco@gmail.com>
2020-08-03 19:09:00 -0300

    Add Repository for the Books

---
+++ src/main/java/com/berga/authorbooks/repository/BookRepository.java
@@ -0,0 +1,51 @@
+package com.berga.authorbooks.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.persistence.EntityManager;
+
+import com.berga.authorbooks.model.Book;
+
+public class BookRepository {
+
+	private EntityManager entityManager;
+
+	public BookRepository(EntityManager entityManager) {
+		this.entityManager = entityManager;
+	}
+
+	@SuppressWarnings("unchecked")
+	public Optional<Book> findById(Integer id) {
+		Book book = entityManager.find(Book.class, id);
+		return (Optional<Book>) (book != null ? Optional.of(book) : Optional.empty());
+	}
+
+	public List<Book> findAll() {
+		return entityManager.createQuery("from Book").getResultList();
+	}
+
+	public Optional<Book> findByName(String name) {
+		Book book = entityManager.createQuery("SELECT b FROM Book b WHERE b.name = :name", Book.class)
+				.setParameter("name", name).getSingleResult();
+		return (Optional<Book>) (book != null ? Optional.of(book) : Optional.empty());
+	}
+
+	public Optional<Book> findByNameNamedQuery(String name) {
+		Book book = entityManager.createNamedQuery("Book.findByName", Book.class).setParameter("name", name)
+				.getSingleResult();
+		return (Optional<Book>) (book != null ? Optional.of(book) : Optional.empty());
+	}
+
+	public Optional<Book> save(Book book) {
+		try {
+			entityManager.getTransaction().begin();
+			entityManager.persist(book);
+			entityManager.getTransaction().commit();
+			return Optional.of(book);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return Optional.empty();
+	}
+}
