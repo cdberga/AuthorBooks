001c2cb8ee528df3663132a78b92f2e3606bd817
Carlos <carlos.bergamasco@gmail.com>
2020-08-04 11:42:43 -0300

    Add constructors and methods to Author class

--- src/main/java/com/berga/authorbooks/model/Author.java
+++ src/main/java/com/berga/authorbooks/model/Author.java
@@ -25,5 +25,42 @@ public class Author {
 	private String name;
 	@OneToMany(mappedBy="author", cascade = CascadeType.ALL)
 	private List<Book> books;
-
+	
+	public Author() {
+		
+	}
+	public Author(String name) {
+		this.name = name;
+	}
+	public Author(Integer id, String name){
+		this.id = id;
+		this.name = name;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public List<Book> getBooks() {
+		return books;
+	}
+	public void addBook(Book book) {
+		books.add(book);
+		book.setAuthor(this);
+	}
+	@Override
+	public String toString() {
+		return "Author{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", books=" + books +
+                '}';
+	}
 }
